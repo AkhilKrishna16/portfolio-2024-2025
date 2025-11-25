4c9c8e0f171ffb9abc6b1188cc0b9c5b85ffbdd8
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-11-25 14:37:18 -0600

    Fixed bug

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,9 +13,9 @@ const Projects = () => {
             key={index}
             project_name={project['name']}
             project_skills={project['skills']}
-            project_description={project['description']}
-            project_image={project['image_link']}
-            project_link={project['project_link']}
+            project_description={project['description'] || ''}
+            project_image={project['image_link'] || ''}
+            project_link={project['project_link'] || ''}
           />
         })}
       </div>
