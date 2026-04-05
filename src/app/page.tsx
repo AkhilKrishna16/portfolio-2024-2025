50a6838fe4be8966c6677133c3bab41857ed69c0
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-05 03:10:49 -0500

    Add stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,8 @@ export default function Home() {
       </div>
 
       <p className="text-sm leading-relaxed text-gray-600 dark:text-zinc-400 mb-16">
-        building with gpus, math, and deep rl. still afraid of posting on x :(
+        love gpus and math, trying to become a low-level demon from scratch.
+        still afraid of posting on x :(
       </p>
 
       <section className="mb-12">
